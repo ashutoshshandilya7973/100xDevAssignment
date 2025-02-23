13b30f59c11fc40ce94a3eb1d41eda1a9b14cfec
Ashu7973 <ashutoshjha7973@gmail.com>
2025-02-23 22:09:02 +0530

    todo functionality is done

--- Week4/ServerTodo/index.js
+++ Week4/ServerTodo/index.js
@@ -1,22 +1,22 @@
 import express from 'express'
 import fs from 'fs'
-const app=express();
+const app = express();
 
 app.use(express.json())
 
 
-app.post('/post',(req,res)=>{
-    let todo=[]
-    const body=req.body;
-    const data=fs.readFileSync('todo.json','utf-8')
-    
-    todo=data?JSON.parse(data):[]
+app.post('/post', (req, res) => {
+    let todo = []
+    const body = req.body;
+    const data = fs.readFileSync('todo.json', 'utf-8')
 
-    const id=todo.length +1;
-    const upload={
-        id:id,
-        title:body.title,
-        isComplete:false
+    todo = data ? JSON.parse(data) : []
+
+    const id = todo.length + 1;
+    const upload = {
+        id: id,
+        title: body.title,
+        isComplete: false
     }
     todo.push(upload);
     try {
@@ -29,40 +29,48 @@ app.post('/post',(req,res)=>{
     }
 })
 
-app.get('/getTodo',(req,res)=>{
-    
-    const data=fs.readFileSync('todo.json','utf-8');
-    const todo=data?JSON.parse(data):[];
+app.get('/getTodo', (req, res) => {
 
-    res.status(201).send({data:todo})
+    const data = fs.readFileSync('todo.json', 'utf-8');
+    const todo = data ? JSON.parse(data) : [];
+
+    res.status(201).send({ data: todo })
 
 })
 
-app.post('/complete',(req,res)=>{
-    const id =Number(req.body.id);
+app.post('/complete', (req, res) => {
+    const id = Number(req.body.id);
 
-    const data=fs.readFileSync('todo.json','utf-8')
-    const todo=data?JSON.parse(data):[];
-    const newTodo=todo.map(item=>{
-        if(item.id===id){
-            item.isComplete=true;
+    const data = fs.readFileSync('todo.json', 'utf-8')
+    const todo = data ? JSON.parse(data) : [];
+    const newTodo = todo.map(item => {
+        if (item.id === id) {
+            item.isComplete = true;
         }
         return item
     })
 
-    fs.writeFileSync('todo.json',JSON.stringify(newTodo,null,2),'utf8')
+    fs.writeFileSync('todo.json', JSON.stringify(newTodo, null, 2), 'utf8')
     console.log("todo status is updated");
-    res.status(200).send({data:newTodo,message:"todo status is updated"})
+    res.status(200).send({ data: newTodo, message: "todo status is updated" })
 
+})
+
+app.post('/todoDelete', (req, res) => {
+    const id = Number(req.body.id);
+    const data = fs.readFileSync('todo.json', 'utf-8');
+    const newdata = data ? JSON.parse(data) : [];
+    const upload = newdata.filter(item => item.id != id);
+    fs.writeFileSync('todo.json', JSON.stringify(upload, null, 2), 'utf-8');
+    console.log("todo is successfully deleted from the file ");
+    res.status(200).send({ data: upload, message: "todo is successfully deleted from the file" })
 })
 
 
 
+const port = 4000;
 
-
-const port =4000;
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`app is running on the server whose port is ${port}`)
 })
 
